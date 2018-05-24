174b1fa9f262f197a40dec397a5602a167231939
Unknown <kumabe.lucia@gmail.com>
2018-05-24 00:54:33 -0300

    6 marcos de lectura

--- Exercise1/Exercise1.pm
+++ Exercise1/Exercise1.pm
@@ -15,27 +15,43 @@ my $io = Bio::SeqIO->new (
 );
 
 my $seq;
+my $prot_obj;
 my $fastaSeq = Bio::Seq->new(
     -seq => '',
     -display_id => '',
     -desc => '',
-    -alphabet => 'dna'
+    -alphabet => 'rna'
 );
 
-while ($seq = $io->next_seq()) {
-    $fastaSeq->desc($seq->description());
-    $fastaSeq->display_id($seq->accession_number());
-    
-    if (defined $seq->seq()) {
-        $fastaSeq->seq($seq->seq());
-    }
-
-    
-}
-
 my $fastaFile = Bio::SeqIO->new(
-        -file => '>'.$fastaSeq->display_id().'.fasta',
+        -file => '>Exercise1.fasta',
         -format => 'fasta'
 );
 
-$fastaFile->write_seq($fastaSeq);
+
+while ($seq = $io->next_seq()) {
+
+    #$fastaSeq->desc($seq->description());
+    #$fastaSeq->display_id($seq->accession_number());
+    
+    if (defined $seq->seq()) {
+        
+        #$fastaSeq->seq($seq->seq());
+        #$fastaFile->write_seq($fastaSeq);
+        
+        #Reading Frames
+        $fastaFile->write_seq($seq->translate(-frame => 0));
+        $fastaFile->write_seq($seq->translate(-frame => 1));
+        $fastaFile->write_seq($seq->translate(-frame => 2));
+
+        #Reading Frames en el orden inverso del complemento
+        $fastaFile->write_seq($seq->revcom(-frame => 0));
+        $fastaFile->write_seq($seq->revcom(-frame => 1));
+        $fastaFile->write_seq($seq->revcom(-frame => 2));
+
+    }
+
+}
+
+
+
